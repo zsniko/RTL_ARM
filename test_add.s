3f89a1c5ab3e50673cab49f842098581abef96b6
Zhifan Song <120737831+zsniko@users.noreply.github.com>
2024-01-17 22:43:06 +0100

    Update test_add.s

--- test_add.s
+++ test_add.s
@@ -1,101 +1,68 @@
-
-@ /*----------------------------------------------------------------
-@ //           test add                                           //
-@ ----------------------------------------------------------------*/
-@ 	.text
-@ 	.globl	_start 
-@ _start:               
-@ 	/* 0x00 Reset Interrupt vector address */
-@ 	b	startup
-	
-@ 	/* 0x04 Undefined Instruction Interrupt vector address */
-@ 	b	_bad
-
-@ startup:
-@ 	mov r0, #0
-@ 	adds r1, r0, r0
-@ 	bne _bad
-@ 	movs r0, #0x80000000
-@ 	bpl _bad
-@ 	subs r0, r0, #1
-@ 	bmi _bad
-@ 	b _good
-
-@ _bad :
-@ 	nop
-@ 	nop
-@ _good :
-@ 	nop
-@ 	nop
-@ AdrStack:  .word 0x80000000
-
-
-
-
-
-
-
-
-
-
-@ /*----------------------------------------------------------------
-@ //           test add        	.text
-	.globl	_start                                    //
-@ ----------------------------------------------------------------*/
-@ 	.text
-@ 	.globl	_start 
-@ _start:               
-@ 	/* 0x00 Reset Interrupt vector address */
-@ 	b	startup
-	
-@ 	/* 0x04 Undefined Instruction Interrupt vector address */
-@ 	b	_bad
-
-@ startup:
-@ 	MOV r0, #12
-@ 	MOV r1, #8 
-@ 	ADD r0, r0, r1 
-@ 	ADD r1, r1, r1 
-@ 	B   _bh 
-@ 	ADD r0, r0, #0
-@ 	ADD r0, r0, #0
-@ 	ADD r0, r0, #0
-
-@ _bh:
-@ 	ADD r1, r1, #2
-@ 	B _good
-
-@ _bad :
-@ 	nop
-@ 	nop
-@ _good :
-@ 	nop
-@ 	nop
-@ AdrStack:  .word 0x80000000
-
-
-	
+@@@@@@@@@@@@@ PGCD @@@@@@@@@@@@@@
 	.text
-	.globl	_start 
+	.globl	_start
 
 _start:
-	mov r0, #5
-	mov r1, #3
+	/* 0x00 Reset Interrupt vector address */
+	b	startup
+
+	/* 0x04 Undefined Instruction Interrupt vector address */
+	b	_bad
+
+startup:
+	MOV r0, #15
+	MOV r1, #9
+	MOV r2, #1
 
 while:
-	cmp r0, r1 
-	BEQ end 
-	SUBGT r1, r1, r0
-	SUBLE r0, r0, r1 
-	B while 
-end:
-	B _good
+	CMP r0, r1
+	BEQ _good 
+	SUBGT r0, r0, r1
+	SUBLE r1, r1, r0
+	B while
 
 _bad :
 	nop
 	nop
+
 _good :
 	nop
 	nop
 
+AdrStack:  .word 0x80000000
 
+
+@ 	.text
+@ 	.globl	_start
+
+@ _start:
+@ 	/* 0x00 Reset Interrupt vector address */
+@ 	b	startup
+
+@ 	/* 0x04 Undefined Instruction Interrupt vector address */
+@ 	b	_bad
+
+@ startup:
+@ 	MOV r0, #15
+@ 	MOV r1, #9
+
+@ while:
+@ 	CMP r0, r1
+@ 	BEQ _good 
+@ 	BGT _supR1
+@ 	SUB r1, r1, r0
+@ 	B while
+
+@ _supR1:
+@ 	SUB r0, r0, r1
+@ 	B while
+
+@ _bad :
+@ 	nop
+@ 	nop
+
+@ _good :
+@ 	nop
+@ 	nop
+
+@ AdrStack:  .word 0x80000000
